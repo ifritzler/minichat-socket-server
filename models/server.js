bd40e4e9434faabe1c00d4c13c8327947cf752a1
Ilan Fritzler <contacto.fritzlerilan@gmail.com>
2022-10-13 14:43:18 -0300

    add main route

--- models/server.js
+++ models/server.js
@@ -23,6 +23,12 @@ class Server {
     this.app.use(express.static(path.resolve(__dirname, "../public")));
   }
 
+  initRoutes() {
+    this.app.get("/", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "../public/index.html"));
+    });
+  }
+
   execute() {
     this.middlewares();
     this.socketsConfig();
